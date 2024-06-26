df9f26a3d442a47846453c75e045c391650f979a
AMD 7700x <7700x@wayland.com>
2024-06-26 23:48:55 +0800

    add pydantic stu code

--- 查询参数.py
+++ 查询参数.py
@@ -12,7 +12,7 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):  # 声明查询参数
-    return fake_items_db[skip : skip + limit]
+    return fake_items_db[skip: skip + limit]
 
 if __name__ == '__main__':
     uvicorn.run("查询参数:app", host='127.0.0.1', port=8000, reload=True)
